aae08cac3bb0059e7bcbe72ea2105ef2f5a79b70
xPaw <me@xpaw.ru>
2013-08-26 21:04:59 +0300

    Add AssemblyInfo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2013, SteamDB. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("SteamDB Updater")]
+[assembly: AssemblyDescription ("The application that keeps SteamDB up to date with the latest changes")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Steam Database")]
+[assembly: AssemblyProduct ("Steam Database")]
+[assembly: AssemblyCopyright ("Copyright © Steam Database, 2013")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.*")]
