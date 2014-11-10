e94e8df432c117645fdb86bb567259803085ce3a
xPaw <github@xpaw.me>
2014-11-10 15:47:53 +0200

    Fix an IRC crash related to new lines

--- Util/Log.cs
+++ Util/Log.cs
@@ -70,7 +70,7 @@ namespace SteamDatabaseBackend
 
             if (!component.Equals("IRC"))
             {
-                IRC.Instance.SendOps("{0}ERROR in {1}:{2} {3}", Colors.RED, component, Colors.NORMAL, string.Format(format.Replace("\n", " "), args));
+                IRC.Instance.SendOps("{0}ERROR in {1}:{2} {3}", Colors.RED, component, Colors.NORMAL, string.Format(format.Replace("\r", "").Replace("\n", " "), args));
             }
         }
 
