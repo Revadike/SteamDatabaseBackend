8f9bc71bfab2abd874429f7b04c6efa0aa7fc01c
Pavel Djundik <github@xpaw.me>
2017-06-15 22:09:30 +0300

    Fix build time check

--- IRC/RSS.cs
+++ IRC/RSS.cs
@@ -119,7 +119,7 @@ namespace SteamDatabaseBackend
                                 continue;
                             }
 
-                            if (DateTime.UtcNow.AddMinutes(10) > build.Date)
+                            if (DateTime.UtcNow > build.Date.AddMinutes(10))
                             {
                                 Log.WriteDebug("RSS", "Got {0} update patch notes, but there is no build within last 10 minutes. {1}", appID, item.Link);
                                 IRC.Instance.SendOps("Got {0} update patch notes, but there is no build within last 10 minutes. {1}", appID, item.Link);
