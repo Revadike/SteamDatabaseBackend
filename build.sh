b24444bd762e924c50f7324d1d0871f86f449a9b
Pavel Djundik <github@xpaw.me>
2017-07-03 23:32:24 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -4,16 +4,6 @@ echo Building Steam Database...
 
 cd "$(dirname "$0")"
 
-rm -r bin/
-cp settings.json.default settings.json
+rm -r bin/ obj/
 
-
-mv Properties/AssemblyInfo.cs assembly.temp
-awk '!/AssemblyInformationalVersion/' assembly.temp > Properties/AssemblyInfo.cs
-
-echo "[assembly: AssemblyInformationalVersion(\"$(git rev-parse --verify HEAD)\")]" >> Properties/AssemblyInfo.cs
-
-nuget restore
-xbuild /p:Configuration=Release
-
-mv assembly.temp Properties/AssemblyInfo.cs
+dotnet build -f netcoreapp2.0 -c Release
