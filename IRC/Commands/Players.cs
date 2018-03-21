4fc3ddab281bf5947d1f506ac9948c54ed9f3a01
Pavel Djundik <github@xpaw.me>
2018-03-21 21:39:48 +0200

    Revert "Don't try to get player count from database in players command"
    This reverts commit 431198180c4fc97b5adcf8ce2917004f453fe957.

--- IRC/Commands/Players.cs
+++ IRC/Commands/Players.cs
@@ -59,6 +59,7 @@ namespace SteamDatabaseBackend
 
                 if (appID == 0)
                 {
+
                     if (!Utils.ConvertUserInputToSQLSearch(ref name))
                     {
                         command.Reply ("Your request is invalid or too short.");
@@ -86,17 +87,25 @@ namespace SteamDatabaseBackend
             {
                 appID = 753;
             }
-            
+
+            var type = "playing";
+            var players = callback.NumPlayers;
             name = Steam.GetAppName(appID, out var appType);
 
             if (callback.Result != EResult.OK)
             {
-                command.Reply($"Unable to request player count for {Colors.BLUE}{name}{Colors.NORMAL}: {Colors.RED}{callback.Result}{Colors.NORMAL} -{Colors.DARKBLUE} {SteamDB.GetAppURL(appID, "graphs")}");
+                using (var db = Database.Get())
+                {
+                    players = await db.ExecuteScalarAsync<uint>("SELECT `CurrentPlayers` FROM `OnlineStats` WHERE `AppID` = @AppID", new { AppID = appID });
+                }
 
-                return;
+                if (players == 0)
+                {
+                    command.Reply($"Unable to request player count for {Colors.BLUE}{name}{Colors.NORMAL}: {Colors.RED}{callback.Result}{Colors.NORMAL} -{Colors.DARKBLUE} {SteamDB.GetAppURL(appID, "graphs")}");
+
+                    return;
+                }
             }
-
-            var type = "playing";
 
             switch (appType)
             {
@@ -127,7 +136,7 @@ namespace SteamDatabaseBackend
 
             command.Reply(
                 "{0}{1:N0}{2} {3} {4}{5}{6}{7} -{8} {9}",
-                Colors.OLIVE, callback.NumPlayers, Colors.NORMAL,
+                Colors.OLIVE, players, Colors.NORMAL,
                 type,
                 Colors.BLUE, name, Colors.NORMAL,
                 callback.Result != EResult.OK ? $" {Colors.RED}({callback.Result}){Colors.NORMAL}" : "",
