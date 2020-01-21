5040f58185dd715806e946178ccd1e0890dbb54f
Pavel Djundik <xPaw@users.noreply.github.com>
2020-01-21 20:58:20 +0200

    Store events in database for deduplication

--- Steam/ClanState.cs
+++ Steam/ClanState.cs
@@ -4,7 +4,9 @@
  * found in the LICENSE file.
  */
 
+using Dapper;
 using SteamKit2;
+using System.Globalization;
 
 namespace SteamDatabaseBackend
 {
@@ -15,7 +17,7 @@ namespace SteamDatabaseBackend
             manager.Subscribe<SteamFriends.ClanStateCallback>(OnClanState);
         }
 
-        private static void OnClanState(SteamFriends.ClanStateCallback callback)
+        private static async void OnClanState(SteamFriends.ClanStateCallback callback)
         {
             if (callback.Events.Count == 0 && callback.Announcements.Count == 0)
             {
@@ -23,8 +25,7 @@ namespace SteamDatabaseBackend
             }
 
             var groupName = callback.ClanName;
-            string message;
-
+            
             if (string.IsNullOrEmpty(groupName))
             {
                 groupName = Steam.Instance.Friends.GetClanName(callback.ClanID);
@@ -38,7 +39,7 @@ namespace SteamDatabaseBackend
 
             foreach (var announcement in callback.Announcements)
             {
-                message = string.Format(
+                var message = string.Format(
                     "{0}{1}{2} announcement: {3}{4}{5} -{6} https://steamcommunity.com/gid/{7}/announcements/detail/{8}",
                     Colors.BLUE, groupName, Colors.NORMAL,
                     Colors.OLIVE, announcement.Headline, Colors.NORMAL,
@@ -50,19 +51,25 @@ namespace SteamDatabaseBackend
                 Log.WriteInfo("Group Announcement", "{0} \"{1}\"", groupName, announcement.Headline);
             }
 
+            using var db = await Database.GetConnectionAsync();
+            
             foreach (var groupEvent in callback.Events)
             {
-                message = string.Format(
-                    "{0}{1}{2} event: {3}{4}{5} -{6} https://steamcommunity.com/gid/{7}/events/{8} {9}({10})",
-                    Colors.BLUE, groupName, Colors.NORMAL,
-                    Colors.OLIVE, groupEvent.Headline, Colors.NORMAL,
-                    Colors.DARKBLUE, callback.ClanID, groupEvent.ID,
-                    Colors.DARKGRAY, groupEvent.EventTime
+                var link = $"https://steamcommunity.com/gid/{callback.ClanID.ConvertToUInt64()}/events/{groupEvent.ID}";
+                var id = await db.ExecuteScalarAsync<int>("SELECT `ID` FROM `RSS` WHERE `Link` = @Link", new { Link = link });
+
+                if (id > 0)
+                {
+                    continue;
+                }
+
+                IRC.Instance.SendMain(
+                    $"{Colors.BLUE}{groupName}{Colors.NORMAL} event: {Colors.OLIVE}{groupEvent.Headline}{Colors.NORMAL} -{Colors.DARKBLUE} {link} {Colors.DARKGRAY}({groupEvent.EventTime.ToString("s", CultureInfo.InvariantCulture).Replace("T", " ")})"
                 );
 
-                IRC.Instance.SendMain(message);
+                Log.WriteInfo("Group Announcement", $"{groupName} Event \"{groupEvent.Headline}\" {link}");
 
-                Log.WriteInfo("Group Announcement", "{0} Event \"{1}\" {3}", groupName, groupEvent.Headline, groupEvent.JustPosted ? "just posted" : "old?");
+                await db.ExecuteAsync("INSERT INTO `RSS` (`Link`, `Title`) VALUES(@Link, @Title)", new { Link = link, Title = groupEvent.Headline });
             }
         }
     }
